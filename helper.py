6c94fa9414fd90df7085e3cc827bffd74a7bb2c3
Mar19845 <mar19845@uvg.edu.gt>
2022-10-30 18:46:49 -0600

    fixes and create folders

---
+++ helper.py
@@ -0,0 +1,14 @@
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from IPython.display import Video
+
+def create_video(row, folder_name,before=5, after=5):
+    print(row["event_attributes"])
+    filename = f"./videos/{folder_name}/test_{row['index']}.mp4"
+    ffmpeg_extract_subclip(
+        f"./videos/train/{row['video_id']}.mp4", 
+        int(row['time']) - before, 
+        int(row['time']) + after, 
+        targetname=filename,
+    )
+    
+    return Video(filename, width=800)
